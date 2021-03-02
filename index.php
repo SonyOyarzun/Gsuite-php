59cc79991edd63b084c5c0acf2e6b084bee68608
SonyOyarzun <46294866+SonyOyarzun@users.noreply.github.com>
2021-03-01 23:32:37 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+error_reporting(0);
+
+require __DIR__ . '/vendor/autoload.php';
+
+if (php_sapi_name() != 'cli') {
+  throw new Exception('This application must be run on the command line.');
+}
+
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+function getClient()
+{
+  $client = new Google_Client();
+  $client->setApplicationName('G Suite Directory API PHP Quickstart');
+  $client->setScopes(Google_Service_Directory::ADMIN_DIRECTORY_USER_READONLY);
+  $client->setAuthConfig('credentials.json');
+  $client->setAccessType('offline');
+  $client->setPrompt('select_account consent');
+
+  // Load previously authorized token from a file, if it exists.
+  // The file token.json stores the user's access and refresh tokens, and is
+  // created automatically when the authorization flow completes for the first
+  // time.
+  $tokenPath = 'token.json';
+  if (file_exists($tokenPath)) {
+    $accessToken = json_decode(file_get_contents($tokenPath), true);
+    $client->setAccessToken($accessToken);
+  }
+
+  // If there is no previous token or it's expired.
+  if ($client->isAccessTokenExpired()) {
+    // Refresh the token if possible, else fetch a new one.
+    if ($client->getRefreshToken()) {
+      $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+    } else {
+      // Request authorization from the user.
+      $authUrl = $client->createAuthUrl();
+      printf("Open the following link in your browser:\n%s\n", $authUrl);
+      print 'Enter verification code: ';
+      $authCode = trim(fgets(STDIN));
+
+      // Exchange authorization code for an access token.
+      $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+      $client->setAccessToken($accessToken);
+
+      // Check to see if there was an error.
+      if (array_key_exists('error', $accessToken)) {
+        throw new Exception(join(', ', $accessToken));
+      }
+    }
+    // Save the token to a file.
+    if (!file_exists(dirname($tokenPath))) {
+      mkdir(dirname($tokenPath), 0700, true);
+    }
+    file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+  }
+  return $client;
+}
+
+
+
+function validate($mail, $client)
+{
+
+  $service = new Google_Service_Directory($client);
+  $user    = new Google_Service_Directory_User();
+  $name    = new Google_Service_Directory_UserName();
+
+
+
+  try {
+    $results = $service->users->get($mail);
+
+    echo $mail;
+    
+  } catch (\Throwable $th) {
+
+    echo " Se Puede Crear : " . $th->getCode();
+
+    switch ($th->getCode()) {
+      case 404:
+       print 'ok';
+        break;
+      
+      default:
+        print $mail = $mail . '-A';
+        validate($mail, $client);
+        break;
+    }
+
+  }
+}
+
+
+$client = getClient();
+
+$mail = 'sony.oyarzun@ugm.cl';
+
+validate($mail, $client);
